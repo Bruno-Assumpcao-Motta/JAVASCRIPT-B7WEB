babfb7757907d459ce67608b1877c6b005dcc9e3
Bruno-Assumpcao-Motta <bruno.assump@gmail.com>
2023-05-24 18:21:09 -0300

    Aula3

---
+++ Primeiros passos/Aula03/assets/js/script.js
@@ -0,0 +1,11 @@
+let nome = "Bruno de Assumpção Motta" /* string */
+let idade = 47
+let logado = true
+let lista = ["ovos","farinha de trigo", "manteiga"]
+let nomecompleto = { nome: "Bruno", idade: 47}
+
+console.log(typeof nome)
+console.log(typeof idade)
+console.log(typeof logado)
+console.log(typeof lista)
+console.log(typeof nomecompleto)
