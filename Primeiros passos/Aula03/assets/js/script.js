3ea07f8e3e9322ff0802d024ac002dad425d18ba
Bruno-Assumpcao-Motta <bruno.assump@gmail.com>
2023-05-24 18:56:54 -0300

    aula3  ex1

--- Primeiros passos/Aula03/assets/js/script.js
+++ Primeiros passos/Aula03/assets/js/script.js
@@ -1,11 +1,12 @@
-let nome = "Bruno de Assumpção Motta" /* string */
-let idade = 47
-let logado = true
-let lista = ["ovos","farinha de trigo", "manteiga"]
-let nomecompleto = { nome: "Bruno", idade: 47}
+let nome = "Bruno de Assumpção Motta" // string 
+let idade = 47 // number
+let logado = true // boolean
+let lista = ["ovos","farinha de trigo", "manteiga"] //array
+let nomecompleto = { nome: "Bruno", idade: 47} // object
 
+// o comando abaixo exibe os dados no console
 console.log(typeof nome)
 console.log(typeof idade)
 console.log(typeof logado)
-console.log(typeof lista)
+console.log(typeof lista) 
 console.log(typeof nomecompleto)
---
+++ Primeiros passos/Aula03/ex01/assets/js/script.js
@@ -0,0 +1,20 @@
+/* Crie uma variavel para armazenar o nome do carro
+e ponha o valor Ferrari.  E Exiba-a */
+
+
+let carro = "Ferrari"
+
+console.log(carro)
+
+/* 2. Crie uma variável para armazenar o preço do bolo,
+que custa R$ 19,90.  E exiba-a */
+
+let valor = "R$ 19,99"
+
+console.log(valor)
+
+/* 3. Corrija o código a seguir e exiba-o.
+let "cidade" = São Paulo */
+
+let cidade = "São Paulo"
+console.log(cidade)
