3c924d065611d64f5880ef90ea1cc8fd2415bc19
Bruno-Assumpcao-Motta <bruno.assump@gmail.com>
2023-05-26 16:17:34 -0300

    Aula05

---
+++ Primeiros passos/Aula05/assets/js/script.js
@@ -0,0 +1,30 @@
+let x = 0;
+x = x + 1
+
+console.log(x)
+
+let nome = 'Bruno';
+let sobrenome = 'Motta';
+let nomeCompleto = nome + ' ' + sobrenome;
+
+console.log(nomeCompleto);
+
+let n1 = 15;
+let n2 = 30;
+
+console.log(n1 + n2)
+
+let n3 = 20;
+let n4 = 12;
+
+let soma = n3 + n4;
+
+console.log(soma)
+
+
+let n5 = 30;
+let n6 = 25;
+let n7 = 12;
+let somadiv = (n5 + n6) - n7
+
+console.log(somadiv)
