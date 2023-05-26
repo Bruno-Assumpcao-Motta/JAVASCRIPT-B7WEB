2a42f03149845a815f39f71f87be1248a232f834
Bruno-Assumpcao-Motta <bruno.assump@gmail.com>
2023-05-26 17:37:27 -0300

    Aula 7 If / else

---
+++ Primeiros passos/Aula07/assets/js/script7.js
@@ -0,0 +1,13 @@
+let idade = 18;
+
+if (idade >= 18) {
+    console.log("Você é MAIOR de idade.")
+} else {
+    console.log("Você é MENOR de idade")
+}
+
+// <  Menor
+// >  Maior
+// == igual
+// >= Maior ou igual
+// <= Menor ou igual
