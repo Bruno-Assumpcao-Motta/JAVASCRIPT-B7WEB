4ab4332606f0dcb7aab7819ff68d8c2974cced14
Bruno-Assumpcao-Motta <bruno.assump@gmail.com>
2023-05-26 16:52:13 -0300

    Aula06

---
+++ Primeiros passos/Aula06/assets/js/script.js
@@ -0,0 +1,13 @@
+let nome = "Bruno";
+let sobrenome = "Motta";
+
+// let nomeCompleto = nome + ' ' + sobrenome; 
+let nomeCompleto = `${nome} de Assumpção ${sobrenome}`
+
+console.log("Nome: " + nomeCompleto);
+
+let idade = 48;
+
+let idadeString = `Idade: ${idade} anos`;
+
+console.log(idadeString);
