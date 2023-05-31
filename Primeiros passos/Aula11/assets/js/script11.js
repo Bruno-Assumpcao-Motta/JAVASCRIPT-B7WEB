67733d704452b3ae433c0b56f1cb764ef0344cb0
Bruno-Assumpcao-Motta <bruno.assump@gmail.com>
2023-05-31 19:29:31 -0300

    Desmistificando o IF

---
+++ Primeiros passos/Aula11/assets/js/script11.js
@@ -0,0 +1,19 @@
+/*
+let ok = true;
+
+if(ok) {
+    console.log("Entrou no IF");
+} else {
+    console.log("Não entrou no IF")
+}
+*/
+
+let idade = 19;
+
+let adulto = idade > 18 && idade < 60;
+
+if (adulto) {
+    console.log("É adulto");
+} else {
+    console.log("Não é adulto")
+}
