0f3bc944af00915687d072f89b445d39b8d6ffd6
Bruno-Assumpcao-Motta <bruno.assump@gmail.com>
2023-06-02 18:55:00 -0300

    Switch

--- Primeiros passos/Aula13/assets/js/script13.js
+++ Primeiros passos/Aula13/assets/js/script13.js
@@ -4,3 +4,10 @@ let shipping = isMember ? 2 : 10
 
 console.log(isMember ? 'Você é membro' : 'Você não é membro')
 console.log("Frete: " + shipping);
+
+
+let age = 20;
+
+let isAdult = (age >= 18 ? 'É adulto' : 'Não é adulto')
+
+console.log(isAdult);
---
+++ Primeiros passos/Aula14/assets/js/script14.js
@@ -0,0 +1,13 @@
+let profession = "fiscal"
+
+switch(profession){
+    case 'fiscal':
+        console.log('Sua camisa será VERDE');
+        break;
+    case 'bombeiro':
+        console.log('Sua camisa será VELHELHA');
+        break;
+    case 'policial':
+        console.log('Sua camisa será AZUL')
+        break;
+}
