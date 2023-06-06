01d5cd3797c4b4fdfb0332ca5cbcab8dba4c6742
Bruno-Assumpcao-Motta <bruno.assump@gmail.com>
2023-06-06 15:45:05 -0300

    Usando funções

--- 2 JavaScript Basico/Aula15/assets/js/script15.js
+++ 2 JavaScript Basico/Aula15/assets/js/script15.js
@@ -2,3 +2,15 @@ function gravidade(){
     console.log('A gravidade do planeta é:');
     console.log('9.8');
 }
+
+console.log('Opa, tudo bem?')
+
+gravidade();
+
+function endereco(){
+    console.log('Moro em Lumiar');
+    console.log('tem 39 meses que estou aqui.')
+}
+
+endereco();
+
