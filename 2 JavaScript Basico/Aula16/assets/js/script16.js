9e7caaf9182ab7421df552430bdc6c7c79ffbc41
Bruno-Assumpcao-Motta <bruno.assump@gmail.com>
2023-06-06 16:07:51 -0300

    Passando parâmetros em funções.

---
+++ 2 JavaScript Basico/Aula16/assets/js/script16.js
@@ -0,0 +1,18 @@
+function somar(n1, n2) {
+    let resultado = n1 + n2;
+    console.log('Resultado: ' +resultado);
+}
+
+somar(15, 30)
+
+function nomeCompleto(nome, sobrenome){
+    console.log(`${nome} ${sobrenome}`);
+}
+nomeCompleto("Bruno", "Motta")
+
+function dataNascimento(dia, mes, ano) {
+    console.log('Nasci em:')
+    console.log(`${dia} ${mes} ${ano}`); 
+    
+}
+dataNascimento(02,10,1975)
