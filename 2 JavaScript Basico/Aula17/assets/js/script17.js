d06a1c6a2ae69621a7b911cecf991b4276ec68bf
Bruno-Assumpcao-Motta <bruno.assump@gmail.com>
2023-06-13 20:05:10 -0300

    Retorno da Função

---
+++ 2 JavaScript Basico/Aula17/assets/js/script17.js
@@ -0,0 +1,9 @@
+function nomeCompleto(nome, sobrenome) {
+    return `${nome} ${sobrenome}` ;
+}
+
+let completo = nomeCompleto("Bruno", "Motta");
+console.log('completo: ' + completo);
+
+
+// ENTRADA -> PROCESSAMENTO -> SAÍDA
