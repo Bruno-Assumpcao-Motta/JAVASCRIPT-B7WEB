d9c69fa2201ee16c3e3a49b291a3aff69e46ac33
Bruno-Assumpcao-Motta <bruno.assump@gmail.com>
2023-06-15 18:09:01 -0300

    Arrow function

---
+++ 2 JavaScript Basico/Aula22/assets/js/script21.js
@@ -0,0 +1,21 @@
+/*function somar(x, y) {
+    return x + y
+}
+
+console.log(somar(10, 5))
+*/
+
+const somar = (x, y) => {
+    return x + y;
+}
+
+console.log(somar(10, 5));
+
+
+/*
+function sobrenome(sob) {
+    return 'Bruno ' + sob;
+}*/
+const sobrenome = sob => 'Bruno' + sob;
+
+console.log(sobrenome('Motta'));
