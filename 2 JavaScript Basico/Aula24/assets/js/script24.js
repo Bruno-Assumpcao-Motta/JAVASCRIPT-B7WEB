88b972895a79659e59defc76ee707ff52d7baaa5
Bruno-Assumpcao-Motta <bruno.assump@gmail.com>
2023-06-21 20:11:45 -0300

    Introdução a Array

---
+++ 2 JavaScript Basico/Aula24/assets/js/script24.js
@@ -0,0 +1,12 @@
+function square (x){
+    return x * x;
+}
+
+
+function addSquares(a, b) {
+    let sqrA = square(a);
+    let sqrB = square(b);
+    return sqrA + sqrB;
+}
+
+console.log(addSquares(2, 3));
