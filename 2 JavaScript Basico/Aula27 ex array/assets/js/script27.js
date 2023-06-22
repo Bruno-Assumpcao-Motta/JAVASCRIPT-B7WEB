e5a0b9f20b942391445867d21c429f193646b259
Bruno-Assumpcao-Motta <bruno.assump@gmail.com>
2023-06-22 20:12:38 -0300

    correcoes e ajustes

---
+++ 2 JavaScript Basico/Aula27 ex array/assets/js/script27.js
@@ -0,0 +1,19 @@
+// 1. No array abaixo, qual o numero que pega a Ferrari?
+let carros = ['BMW', 'Ferrari', 'Mercedes'];
+let x = 1
+console.log('1. ' + carros[x]);
+
+
+// 2. Troque a Ferrari por Audi
+carros[1] = 'Audi'
+console.log('2 Lista com Audi:');
+console.log(carros);
+
+// 3. Adicione o Volvo a lista
+carros.push('Volvo');
+console.log('3. Lista com Volvo:');
+console.log(carros);
+
+// 4.Exista quantos itens tem no array
+console.log('4. itens no array: ');
+console.log(carros.length)
