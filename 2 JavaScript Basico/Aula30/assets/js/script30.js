c045687cb9058b72c135e16e90d44c1f05d099e4
Bruno-Assumpcao-Motta <bruno.assump@gmail.com>
2023-06-27 18:17:05 -0300

    Aula30

---
+++ 2 JavaScript Basico/Aula30/assets/js/script30.js
@@ -0,0 +1,10 @@
+let personagem = {
+    nome: 'Bruno',
+    idade: 50,
+    carros: [
+        {modelo: 'Fiat', cor: 'preto' },
+        {modelo: 'Citroen', cor: 'azul' }
+    ]
+}
+
+console.log(personagem.carros[1].modelo);
