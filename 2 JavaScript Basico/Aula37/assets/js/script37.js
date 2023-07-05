169cca36fecd7df61d64c4981e9acd1c111efb86
Bruno-Assumpcao-Motta <bruno.assump@gmail.com>
2023-07-05 18:43:09 -0300

    Aula 37 ordenação de array

--- 2 JavaScript Basico/Aula37/assets/js/script37.js
+++ 2 JavaScript Basico/Aula37/assets/js/script37.js
@@ -4,3 +4,15 @@ fruits.sort();
 fruits.reverse();
 
 console.log(fruits);
+
+
+let cars = [
+    {brand: 'Fiat', year:2022},
+    {brand: 'Chevrolet', year:2021},
+    {brand: 'Ford', year:2019},
+    {brand: 'Peugeot', year:2020}
+]
+
+cars.sort((a,b) => a.year - b.year);
+cars.reverse();
+console.log(cars)
