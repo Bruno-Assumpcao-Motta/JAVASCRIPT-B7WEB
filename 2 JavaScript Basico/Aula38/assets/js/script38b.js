e783687464e16704e13c222c82e20783b2277dcc
Bruno-Assumpcao-Motta <bruno.assump@gmail.com>
2023-07-05 18:59:37 -0300

    Aula 38b

---
+++ 2 JavaScript Basico/Aula38/assets/js/script38b.js
@@ -0,0 +1,12 @@
+let fruits = ['Banana', 'Laranja', 'Maça', 'Pêra', 'Uva'];
+
+let ok = fruits.some((value) => {
+    return value.length > 3;
+});
+
+if (ok) {
+    console.log('Algum item é maior que 3');
+} else {
+    console.log('Nenhum item é maior que 3')
+}
+
